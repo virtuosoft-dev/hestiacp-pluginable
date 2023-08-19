57ce4937ebcc2be9e879596ccffa187819d95655
Stephen J Carnam <steve@steveorevo.com>
2023-08-19 14:55:23 -0700

    typo

--- hooks/post_install.sh
+++ hooks/post_install.sh
@@ -12,7 +12,7 @@
 
 // Copy pluginable.php to /usr/local/hestia/web/pluginable.php
 copy( '/etc/hestiacp/hooks/pluginable.php', '/usr/local/hestia/web/pluginable.php' );
-shell_exec( __DIR__ . '/install_hcpp_reboot.sh' && cd /usr/local/hestia/web/js/dist && wget https://code.jquery.com/jquery-3.7.0.min.js' );
+shell_exec( __DIR__ . '/install_hcpp_reboot.sh && cd /usr/local/hestia/web/js/dist && wget https://code.jquery.com/jquery-3.7.0.min.js' );
 
 // Ensure log is present and writable when needed
 if ( ! file_exists( '/tmp/hcpp.log' ) ) {
